21bce106971c3a3444cbf2ffa9222eb82733ebf8
“sarthak <sarthakgangrade0@gmail.com>
2021-10-19 23:55:16 +0530

    add UC4

---
+++ uc4/uc4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("emp Wage: " + empWage);
+            Console.ReadKey();
+        }
+    }
+}
